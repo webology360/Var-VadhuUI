a4e6ae69b7a081e416b753fa52787a0e3a071d78
galibgaurav <galibgaurav@gmail.com>
2023-07-15 12:52:13 +0530

    sample biodata and images

--- src/api/apiConstants.js
+++ src/api/apiConstants.js
@@ -1,6 +1,6 @@
 export const apiContants = {
-  BASE_URL_1: "http://localhost:5000/api/",
-  //BASE_URL_1: "https://varvadhuservices.onrender.com/api/",
+  //BASE_URL_1: "http://localhost:5000/api/",
+  BASE_URL_1: "https://varvadhuservices.onrender.com/api/",
   ADMIN: {
     LOGIN: "admin/login",
     SIGNUP: "admin/signup",
