18c8f3cf3daea87f4efa58f890fb1203537487a3
galibgaurav <galibgaurav@gmail.com>
2023-08-10 00:29:37 +0530

    commented connect modal just to make thing simple as of now, will implement correct behaviour in future

--- src/components/ConnectModal/ConnectModal.jsx
+++ src/components/ConnectModal/ConnectModal.jsx
@@ -9,17 +9,29 @@ const ConnectModal = ({ connectModal, handleConnectModal, userId }) => {
       isOpen={connectModal}
       height={35}
       width={50}
-      heading="Please Enter Below Details"
+      heading=""
       toggle={handleConnectModal}
       paddingBody="0rem 3rem"
-      //   buttonLabel1="Cancel"
-      //   buttonLabel2="Connect"
-      //   handleSubmit={handleSubmitConnectModal}
+    //   buttonLabel1="Cancel"
+    //   buttonLabel2="Connect"
+    //   handleSubmit={handleSubmitConnectModal}
     >
-      <ConnectModalForm
+      {/* <ConnectModalForm
         handleConnectModal={handleConnectModal}
         userId={userId}
-      />
+      /> */}
+      <div>
+        <div style={{fontFamily:"cursive",fontSize:"2rem"}}>
+          <p>
+           Send "Hi" on whatsapp 6207406637 OR Call on 6207406637 to get contact information
+          </p>
+          <br/>
+          <p>
+           संपर्क जानकारी प्राप्त करने के लिए व्हाट्सएप 6207406637 पर "नमस्ते" भेजें या 6207406637 पर कॉल करें
+          </p>
+        </div>
+       </div>
+
     </Modal>
   );
 };
